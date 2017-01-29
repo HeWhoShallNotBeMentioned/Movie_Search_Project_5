b7f86672f7d2487a6850c542173d602236fff0c1
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-29 15:50:12 -0800

    finished the li function for displaying movies

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $.getJSON('http://www.omdbapi.com/?', {"s": "potter", "y": "2016", "r": "json"}, function(response) {
+    $.getJSON('http://www.omdbapi.com/?', {"s": "star wars", "y": "", "r": "json"}, function(response) {
       console.log(response);
       var statusHTML = "";
-      $.each(response, function(index, movie){
+      $.each(response.Search, function(index, movie){
         statusHTML += "<li>";
         // console.log(movie.Poster);
         // console.log(movie.Title);
@@ -18,6 +18,12 @@ $(document).ready(function(){
           statusHTML += '</div>';
         }
         //statusHTML += "will this show?";
+        statusHTML += '<span class="movie-title">';
+        statusHTML += movie.Title;
+        statusHTML += '</span>';
+        statusHTML += '<span class="movie-year">';
+        statusHTML += movie.Year;
+        statusHTML += '</span>';
         statusHTML += "</li>";
 
       });
