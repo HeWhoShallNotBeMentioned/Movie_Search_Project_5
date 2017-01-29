22ab3c983f44c78aba1e09a088c9e4d67773eeb4
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-28 21:56:51 -0800

    add first draft of code to process JSON and print it to the pages. Has errors

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
+$(document).ready(function(){
+    $.getJSON('http://www.omdbapi.com/?', {"s": "potter", "y": "2016", "r": "json"}, function(response) {
+      console.log(response);
+      var statusHTML = "";
+      $.each(response, function(index, movie){
+        statusHTML += "<li>";
+        // console.log(movie.Poster);
+        // console.log(movie.Title);
+        if(movie.Poster !== "N/A"){
+          statusHTML += '<div class="poster-wrap">';
+          statusHTML += '<img class="movie-poster" src="' ;
+          statusHTML += movie.Poster;
+          statusHTML +=  '">';
+          statusHTML += '</div>';
+        } else {
+          statusHTML += '<div class="poster-wrap">';
+          statusHTML += '<i class="material-icons poster-placeholder">crop_original</i>';
+          statusHTML += '</div>';
+        }
+        //statusHTML += "will this show?";
+        statusHTML += "</li>";
 
-$.get('http://www.omdbapi.com/?', {"s": "star wars", "y": "1977"}, function(response) {
-  console.log(response);
+      });
+    $('#movies').html(statusHTML);
+  });
 });
